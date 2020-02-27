f1899a614344581988cdad4fe10ff25c98101a9e
José Parera <jose.parera@upm.es>
2020-02-27 19:12:39 +0100

    latex fixes

--- scikit-dsp/skdsp/signal/printer.py
+++ scikit-dsp/skdsp/signal/printer.py
@@ -7,15 +7,22 @@ import skdsp.signal.signal
 class SignalLatexPrinter(LatexPrinter):
     def print_signal(self, sg):
         ltx = super().doprint(sg.amplitude)
+        return self.apply_rules(ltx, sg.iv)
+
+    def print_expr(self, expr, iv):
+        ltx = super().doprint(expr)
+        return self.apply_rules(ltx, iv)
+
+    def apply_rules(self, ltx, iv):
         rules = inspect.getmembers(self, inspect.ismethod)
         for rule in rules:
             if rule[0].startswith("rule"):
-                ltx = rule[1](ltx, sg)
+                ltx = rule[1](ltx, iv)
         return ltx
 
-    def rule_iv_in_frac(self, ltx, sg):
+    def rule_iv_in_frac(self, ltx, iv):
         # extract iv from frac
-        pat = r"(.*?)?(\\frac{)(.*)(" + "{0}".format(sg.iv.name) + ")(}{.+?})(.*)?"
+        pat = r"(.*?)?(\\frac{)(.*)(" + "{0}".format(iv.name) + ")(}{.+?})(.*)?"
         fr = re.compile(pat)
         m = fr.match(ltx)
         if m is not None:
@@ -27,7 +34,7 @@ class SignalLatexPrinter(LatexPrinter):
             return s
         return ltx
 
-    def rule_j_in_frac(self, ltx, _sg):
+    def rule_j_in_frac(self, ltx, iv):
         pat = r"(.*?)?(\\frac{)(\\mathrm{j}*)(.*)(}{.+?})(.*)?"
         fr = re.compile(pat)
         m = fr.match(ltx)
@@ -35,7 +42,7 @@ class SignalLatexPrinter(LatexPrinter):
             return "".join(m.group(1, 3, 2, 4, 5, 6))
         return ltx
 
-    def rule_exp(self, ltx, _sg):
+    def rule_exp(self, ltx, iv):
         pat = r"(.*?)?(e\^{)(.*)"
         fr = re.compile(pat)
         m = fr.match(ltx)
@@ -44,3 +51,14 @@ class SignalLatexPrinter(LatexPrinter):
             return s
         return ltx
 
+
+def latex(expr, iv):
+    printer = SignalLatexPrinter(
+        settings={
+            'imaginary_unit': 'rj',
+            'fold_frac_powers': True,
+            'order': 'lex',
+            'gothic_re_im': True,
+        }
+    )
+    return printer.print_expr(expr, iv)
