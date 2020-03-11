84044d92cd0bbbec79a7f2abc02b9802d4810ff2
José Parera <jose.parera@upm.es>
2020-03-11 13:29:14 +0100

    moved tests

--- scikit-dsp/skdsp/tests/test_system_discrete.py
+++ scikit-dsp/skdsp/tests/test_system_discrete.py
@@ -1,8 +1,10 @@
+import numpy as np
 import pytest
 import sympy as sp
+from sympy.solvers.ode import ode_nth_linear_constant_coeff_homogeneous
 
-from skdsp.signal.discrete import Constant, Delta, Exponential, Step, n
-from skdsp.system.discrete import Delay, DiscreteSystem, Identity, Zero
+from skdsp.signal.discrete import Constant, Data, Delta, Exponential, Step, n
+from skdsp.system.discrete import Delay, DiscreteSystem, Identity, Zero, filter
 
 
 class Test_Zero(object):
@@ -25,7 +27,7 @@ class Test_Zero(object):
 
     def test_Zero_impulse_response(self):
         S = Zero()
-        h = S.impulse_response
+        h = S.impulse_response()
         assert h == Constant(0)
 
 
@@ -49,7 +51,7 @@ class Test_Identity(object):
 
     def test_Identity_impulse_response(self):
         S = Identity()
-        h = S.impulse_response
+        h = S.impulse_response()
         assert h == Delta()
 
 
@@ -82,5 +84,62 @@ class Test_Delay(object):
 
     def test_Delay_impulse_response(self):
         S = Delay(3)
-        h = S.impulse_response
+        h = S.impulse_response()
         assert h == Delta().shift(3)
+
+
+
+class Test_KK(object):
+
+    def test_filter(self):
+        y3 = Exponential(
+            sp.exp(sp.I * sp.S.Pi / 4), 1.1 * sp.exp(sp.I * sp.S.Pi / 11)
+        )
+        G = y3.phasor
+        z0 = y3.alpha
+        x = G * Delta()
+        span = range(0, 51)
+        B = [1, 0]
+        A = [1, -z0]
+        y3f = Data(filter(B, A, x(span)), iv=n)
+
+    def test_dsolve(self):
+        f = sp.Function("f")
+        y = sp.Function("y")
+        # ode = sp.Eq(sp.S(1)/8*f(n).diff(n, 2) - sp.S(3)/4*f(n).diff(n) + f(n), 10)
+        ode = sp.Eq(f(n).diff(n, 2) - sp.S(3)/4*f(n).diff(n) + sp.S(1)/8*f(n), 0)
+        # jint = 'nth_linear_constant_coeff_undetermined_coefficients'
+        jint = 'nth_linear_constant_coeff_homogeneous'
+        # sol = sp.dsolve(ode, f(n), hint=jint)
+        lccde_out = y(n) - sp.S(3)/4*y(n-1) + sp.S(1)/8*y(n-2)
+        coeffs = {0: sp.S(1)/8, 1: -sp.S(3)/4, 2: 1}
+        sol = ode_nth_linear_constant_coeff_homogeneous(lccde_out, f(n), len(coeffs)-1, coeffs, returns="sol")
+        assert sol is not None
+
+    def test_recursion(self):
+        x = sp.S(3)/10*Delta() + sp.S(6)/10*Delta().shift(1) + sp.S(3)/10*Delta().shift(2)
+        y = [0, 0]
+        for k in range(0, 128):
+            y.append(-sp.S(9)/10*y[k-2+2] + x(k))
+        assert y is not None
+
+        B = [sp.S(3) / 10, sp.S(6) / 10, sp.S(3) / 10]
+        A = [1, 0, sp.S(9) / 10]
+        Y = filter(B, A, Delta()(range(0, 129)))
+        assert Y is not None
+
+        x = Delta()
+        xv = x[-2:51]
+        yv = [0, 0]
+        span = range(0, 50)
+        for m in span:
+            v = -A[2]*yv[m] + B[0]*xv[m+2] + B[1]*xv[m+1] + B[2]*xv[m]
+            yv.append(v)
+        from skdsp.util.util import ipystem
+        ipystem(span, np.array([float(y) for y in yv[2:]]), title=r'$h[n]$');
+
+    def test_roots(self):
+        x = sp.Symbol('x')
+        poly = sp.Poly(x**2 - 2*x + 1) 
+        res = sp.roots(poly)
+        assert res is not None
