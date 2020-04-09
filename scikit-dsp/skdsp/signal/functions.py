b59a4bd21d0b1c0f653dca1e8c6404342a7b0aef
José Parera <jose.parera@upm.es>
2020-04-09 11:39:51 +0200

    iv (meaningless) to args

--- scikit-dsp/skdsp/signal/functions.py
+++ scikit-dsp/skdsp/signal/functions.py
@@ -35,8 +35,8 @@ class UnitDelta(sp.KroneckerDelta):
         return self
 
     def _eval_subs(self, old, new):
-        if old == self.iv:
-            return UnitDelta(new)
+        if self.func_arg.has(old):
+            return UnitDelta(self.func_arg.subs(old, new))
 
     def __new__(cls, iv):
         obj = super().__new__(sp.KroneckerDelta, sp.S.Zero, iv)
@@ -45,14 +45,14 @@ class UnitDelta(sp.KroneckerDelta):
         return obj
 
     def _eval_rewrite_as_UnitStep(self, *_args, **_kwargs):
-        return UnitStep(self.iv) - UnitStep(self.iv - 1)
+        return UnitStep(self.func_arg) - UnitStep(self.func_arg - 1)
 
     def _eval_rewrite_as_Piecewise(self, *_args, **_kwargs):
-        return sp.Piecewise((1, sp.Eq(self.iv, 0)), (0, True))
+        return sp.Piecewise((1, sp.Eq(self.func_arg, 0)), (0, True))
 
     @property
     def func(self):
-        callers = ['lambdify']
+        callers = ["lambdify"]
         stack = inspect.stack()
         for frame in stack:
             if frame.function in callers:
@@ -60,7 +60,7 @@ class UnitDelta(sp.KroneckerDelta):
         return sp.KroneckerDelta
 
     @property
-    def iv(self):
+    def func_arg(self):
         return self.args[1]
 
     @staticmethod
@@ -69,16 +69,16 @@ class UnitDelta(sp.KroneckerDelta):
         return np.equal(n, 0).astype(np.float_)
 
     def __str__(self):
-        return "UnitDelta({0})".format(self.iv)
+        return "UnitDelta({0})".format(self.func_arg)
 
     def __repr__(self):
-        return "\u03b4[{0}]".format(self.iv)
+        return "\u03b4[{0}]".format(self.func_arg)
 
     def _sympystr(self, printer=None):
-        return r"UnitDelta({0})".format(printer.doprint(self.iv))
+        return r"UnitDelta({0})".format(printer.doprint(self.func_arg))
 
     def _latex(self, printer=None):
-        return r"\delta\left[{0}\right]".format(printer.doprint(self.iv))
+        return r"\delta\left[{0}\right]".format(printer.doprint(self.func_arg))
 
 
 class UnitStep(sp.Function):
@@ -106,20 +106,20 @@ class UnitStep(sp.Function):
         return self
 
     @property
-    def iv(self):
+    def func_arg(self):
         return self.args[0]
 
     def _eval_rewrite_as_Piecewise(self, *_args, **_kwargs):
-        return sp.Piecewise((1, self.iv >= 0), (0, True))
+        return sp.Piecewise((1, self.func_arg >= 0), (0, True))
 
     def _eval_rewrite_as_UnitDelta(self, *_args, **kwargs):
         k = sp.Dummy(integer=True)
-        if kwargs and kwargs.get('form', 'None') == 'accum':
-            return sp.Sum(UnitDelta(k), (k, sp.S.NegativeInfinity, self.iv))
-        return sp.Sum(UnitDelta(self.iv - k), (k, 0, sp.S.Infinity))
+        if kwargs and kwargs.get("form", "None") == "accum":
+            return sp.Sum(UnitDelta(k), (k, sp.S.NegativeInfinity, self.func_arg))
+        return sp.Sum(UnitDelta(self.func_arg - k), (k, 0, sp.S.Infinity))
 
     def _eval_rewrite_as_UnitRamp(self, *_args, **_kwargs):
-        return UnitRamp(self.iv + 1) - UnitRamp(self.iv)
+        return UnitRamp(self.func_arg + 1) - UnitRamp(self.func_arg)
 
     def _eval_difference_delta(self, var, step):
         # f[n+step] - f[n]
@@ -128,9 +128,9 @@ class UnitStep(sp.Function):
             if step == 0:
                 return sp.S.Zero
             elif step == 1:
-                return UnitDelta(self.iv)
+                return UnitDelta(self.func_arg)
             elif step == -1:
-                return -UnitDelta(self.iv)
+                return -UnitDelta(self.func_arg)
         return None
 
     @staticmethod
@@ -138,21 +138,22 @@ class UnitStep(sp.Function):
         return np.greater_equal(n, 0).astype(np.float_)
 
     def __str__(self):
-        return "UnitStep({0})".format(self.iv)
+        return "UnitStep({0})".format(self.func_arg)
 
     def __repr__(self):
-        return "u[{0}]".format(self.iv)
+        return "u[{0}]".format(self.func_arg)
 
     def _sympystr(self, printer=None):
-        return r"UnitStep({0})".format(printer.doprint(self.iv))
+        return r"UnitStep({0})".format(printer.doprint(self.func_arg))
 
     def _latex(self, printer=None):
-        return r"u\left[{0}\right]".format(printer.doprint(self.iv))
+        return r"u\left[{0}\right]".format(printer.doprint(self.func_arg))
 
     def doit(self, *args, **kwargs):
-        if kwargs and kwargs.get('piecewise', False):
+        if kwargs and kwargs.get("piecewise", False):
             return self._eval_rewrite_as_Piecewise()
         return self
+
 
 class UnitRamp(sp.Function):
 
@@ -173,44 +174,44 @@ class UnitRamp(sp.Function):
 
     def _eval_power(self, other):
         # other is not Nan, 0 or 1
-        return self.iv ** other * UnitStep(self.iv)
+        return self.func_arg ** other * UnitStep(self.func_arg)
 
     @property
-    def iv(self):
+    def func_arg(self):
         return self.args[0]
 
     def _eval_rewrite_as_Piecewise(self, *_args, **_kwargs):
-        # return sp.Piecewise((self.iv, self.iv >= 0), (0, True))
-        return sp.Piecewise((self.iv, self.iv > 0), (0, True))
+        # return sp.Piecewise((self.func_arg, self.func_arg >= 0), (0, True))
+        return sp.Piecewise((self.func_arg, self.func_arg > 0), (0, True))
 
     def _eval_rewrite_as_UnitStep(self, *_args, **kwargs):
         k = sp.Dummy(integer=True)
         if "form" in kwargs and kwargs["form"] == "accum":
-            return sp.Sum(UnitStep(k - 1), (k, sp.S.NegativeInfinity, self.iv))
+            return sp.Sum(UnitStep(k - 1), (k, sp.S.NegativeInfinity, self.func_arg))
         else:
-            return self.iv * UnitStep(self.iv)
+            return self.func_arg * UnitStep(self.func_arg)
 
     def _eval_rewrite_as_Max(self, *_args, **_kwargs):
-        return sp.Max(0, self.iv)
+        return sp.Max(0, self.func_arg)
 
     def _eval_rewrite_as_Abs(self, *_args, **_kwargs):
-        return sp.S.Half * (self.iv + sp.Abs(self.iv))
+        return sp.S.Half * (self.func_arg + sp.Abs(self.func_arg))
 
     @staticmethod
     def _imp_(n):
         return n * np.greater_equal(n, 0).astype(np.float_)
 
     def __str__(self):
-        return "UnitRamp({0})".format(self.iv)
+        return "UnitRamp({0})".format(self.func_arg)
 
     def __repr__(self):
-        return "r[{0}]".format(self.iv)
+        return "r[{0}]".format(self.func_arg)
 
     def _sympystr(self, printer=None):
-        return r"UnitRamp({0})".format(printer.doprint(self.iv))
+        return r"UnitRamp({0})".format(printer.doprint(self.func_arg))
 
     def _latex(self, printer=None):
-        return r"r\left[{0}\right]".format(printer.doprint(self.iv))
+        return r"r\left[{0}\right]".format(printer.doprint(self.func_arg))
 
 
 class UnitDeltaTrain(sp.Function):
@@ -233,7 +234,7 @@ class UnitDeltaTrain(sp.Function):
             return sp.S.Zero
 
     @property
-    def iv(self):
+    def func_arg(self):
         return self.args[0]
 
     @property
@@ -241,7 +242,7 @@ class UnitDeltaTrain(sp.Function):
         return self.args[1]
 
     def _eval_rewrite_as_Piecewise(self, *_args, **_kwargs):
-        nm = sp.Eq(sp.Mod(self.iv, self.N), 0)
+        nm = sp.Eq(sp.Mod(self.func_arg, self.N), 0)
         return sp.Piecewise((1, nm), (0, True))
 
     @staticmethod
@@ -249,35 +250,33 @@ class UnitDeltaTrain(sp.Function):
         return np.equal(np.mod(n, N), 0).astype(np.float_)
 
     def __str__(self):
-        return "UnitDeltaTrain({0}, {1})".format(self.iv, self.N)
+        return "UnitDeltaTrain({0}, {1})".format(self.func_arg, self.N)
 
     def __repr__(self):
-        return "\u0428[(({0})){1}]".format(self.iv, self.N)
+        return "\u0428[(({0})){1}]".format(self.func_arg, self.N)
 
     def _sympystr(self, printer=None):
         return r"UnitDeltaTrain({0}, {1})".format(
-            printer.doprint(self.iv), printer.doprint(self.N)
+            printer.doprint(self.func_arg), printer.doprint(self.N)
         )
 
     def _latex(self, printer=None):
         return r"{{\rotatebox[origin=c]{{180}}{{$\Pi\kern-0.361em\Pi$}}\left[(({0}))_{{{1}}}\right]".format(
-            printer.doprint(self.iv), printer.doprint(self.N)
+            printer.doprint(self.func_arg), printer.doprint(self.N)
         )
+
 
 def stepsimp(expr):
     if isinstance(expr, sp.Mul):
-        cnt = 0
-        for arg in expr.args:
-            if arg.func == UnitStep:
-                cnt += 1
+        cnt = len(expr.atoms(UnitStep))
         if cnt >= 2:
             expr = sp.simplify(expr.rewrite(sp.Piecewise))
             if isinstance(expr, sp.Piecewise):
                 if expr.args[1] == (0, True):
-                    A = sp.Wild('A')
-                    s = sp.Wild('s')
-                    k = sp.Wild('k')
-                    l = sp.Wild('l')
+                    A = sp.Wild("A")
+                    s = sp.Wild("s")
+                    k = sp.Wild("k")
+                    l = sp.Wild("l")
                     m = expr.args[0].match((A, s >= k))
                     if m:
                         return m[A] * UnitStep(m[s] - m[k])
@@ -289,9 +288,9 @@ def stepsimp(expr):
                         e = m[A] * (UnitStep(m[s] - m[k]) - UnitStep(m[s] - (m[l] + 1)))
                         return sp.Piecewise((e, m[l] >= 0), (0, True))
     else:
-        e = sp.Wild('e')
-        iv = sp.Wild('iv')
-        k = sp.Wild('k')
+        e = sp.Wild("e")
+        iv = sp.Wild("iv")
+        k = sp.Wild("k")
         m = expr.match(sp.Piecewise((e, iv >= k), (0, True)))
         if m:
             expr = m[e] * UnitStep(m[iv] - m[k])
@@ -301,3 +300,10 @@ def stepsimp(expr):
             expr = m[e] * UnitStep(m[iv] - m[k])
             return expr
     return expr
+
+
+def deltasimp(expr, iv):
+    # prefer d[n-1] to d[-n+1]
+    k = sp.Wild("k")
+    newexpr = expr.replace(UnitDelta(-iv + k), UnitDelta(iv - k))
+    return newexpr
